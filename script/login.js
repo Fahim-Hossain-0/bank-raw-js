38434399507254a150b8213c24fd3d9a704f7cd5
fahim-hossain-0 <fahimhossain735@gmail.com>
2025-03-01 00:58:51 +0600

    add

--- script/login.js
+++ script/login.js
@@ -1,19 +1,21 @@
-document.getElementById('login-btn').addEventListener("click",function(event){
+document.getElementById('login-btn').addEventListener("click", function (event) {
     event.preventDefault()
     const accountNumber = document.getElementById('account-number').value.trim();
     // console.log(typeof accountNumber);
     const pinNumber = document.getElementById('pin').value.trim();
-    // console.log(typeof pinNumber);
-    if(accountNumber.length === 11){
-        if(pinNumber.length === 1234){
+    const convertedPinNumber = parseInt(pinNumber)
+    console.log(typeof convertedPinNumber);
+    if (accountNumber.length === 11) {
+        if (convertedPinNumber === 1234) {
+
             window.location.href = "/main.html"
         }
-        else{
+        else {
             alert('Incorrect Pin')
         }
-        
+
     }
-    else{
+    else {
         alert('account number not right')
     }
 })
